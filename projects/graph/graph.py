8eb78670447124bcfd434531a19dda0c82ac3d3d
tomexar <tommyexar@gmail.com>
2019-11-05 15:15:23 -0700

    MVP

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -11,45 +11,120 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+
+        self.vertices[vertex] = set()
+
+
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 not in self.vertices:
+            self.vertices.add_vertex(v1)
+
+        if v2 not in self.vertices:
+            self.vertices.add_vertex(v2)
+        
+        self.vertices[v1].add(v2)
+
+
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        queue = Queue()
+        visited = set()
+        path = []
+        queue.enqueue(starting_vertex)
+        while queue.size():
+            node = queue.dequeue()
+            path.append(node)
+            visited.add(node)
+            for edge in self.vertices[node]:
+                if edge not in visited:
+                    queue.enqueue(edge)
+            print('bft', path)
+
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
-    def dft_recursive(self, starting_vertex):
+        stack = Stack()
+        visited = set()
+        path = []
+        stack.push(starting_vertex)
+        while stack.size():
+            node = stack.pop()
+            path.append(node)
+            visited.add(node)
+            for edge in self.vertices[node]:
+                if edge not in visited:
+                    visited.add(edge)
+                    stack.push(edge)
+        print("DFT:", path)
+
+    def dft_recursive(self, starting_vertex, visited = set()):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         This should be done using recursion.
         """
-        pass  # TODO
+        if starting_vertex in visited:
+            return visited
+        else:
+            visited.add(starting_vertex)
+            for neighbor in self.vertices[starting_vertex]:
+                return self.dft_recursive(neighbor, visited)
+
+        print('recursive', path)
+
+
     def bfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing the shortest path from
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        queue = Queue()
+        visited = set()
+        queue.enqueue([starting_vertex])
+        while queue.size() > 0:
+            path = queue.dequeue()
+            node = path[-1]
+            if node not in visited:
+                if node == destination_vertex:
+                    return path
+                else:
+                    visited.add(node)
+                    for connection in self.vertices[node]:
+                        copy_path = path[:]
+                        copy_path.append(connection)
+                        queue.enqueue(copy_path)  
+
+
     def dfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        pass  # TODO
+        stack = Stack()
+        visited = set()
+        stack.push([starting_vertex])
+        while stack.size():
+            path = stack.pop()
+            node = path[-1]
+            if node is not visited:
+                if node == destination_vertex:
+                    return path
+                else:
+                    visited.add(node)
+                    for connection in self.vertices[node]:
+                        copy_path = path[:]
+                        copy_path.append(connection)
+                        stack.push(copy_path)
 
 
 
@@ -86,7 +161,7 @@ if __name__ == '__main__':
     Valid DFT paths:
         1, 2, 3, 5, 4, 6, 7
         1, 2, 3, 5, 4, 7, 6
-        1, 2, 4, 7, 6, 3, 5
+        1, 2, 4, 7, 6, 3, 5**
         1, 2, 4, 6, 3, 5, 7
     '''
     graph.dft(1)
